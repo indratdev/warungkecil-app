2b149d5be61c1c27b48075444e94a65fe03d1092
indratdev <indrat.dev@gmail.com>
2024-05-03 15:37:37 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+
+class AuthController extends Controller
+{
+    public function index()
+    {
+
+        if (Auth::check()) {
+            return redirect('admin.admin');
+        } else {
+            return view('login.login');
+        }
+    }
+
+    public function actionlogin(Request $request)
+    {
+        $data = [
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+        ];
+
+        if (Auth::Attempt($data)) {
+            return redirect('/home');
+        } else {
+
+            Session::flash('error', 'Email atau Password Salah');
+            return redirect('/');
+        }
+    }
+
+    public function actionlogout()
+    {
+        Auth::logout();
+        return redirect('/');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/admin', 'AdminController@index');
+
+Route::get('/', [AuthController::class, 'index']);
+
+Route::post('actionlogin', [AuthController::class, 'actionlogin'])->name('actionlogin');
+
+Route::get('/logout', [AuthController::class, 'actionlogout']);
+
+Route::get('/home', [AdminController::class, 'index']);
