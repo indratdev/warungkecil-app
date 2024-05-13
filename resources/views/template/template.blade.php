fd1f6880af76a3f43f990a3d5fce94250264a0f5
indratdev <indrat.dev@gmail.com>
2024-05-13 23:07:37 +0700

    add: edit form project

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
-<html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title')</title>
-    @include('master.include.style')
-
-
-</head>
-
-<body class="hold-transition sidebar-mini">
-    <div class="wrapper">
         <!-- Navbar -->
         @include('master.header')
 
@@ -63,13 +46,4 @@ scratch. This page gets rid of all links and provides the needed markup only.
         </aside>
         <!-- /.control-sidebar -->
 
-        <!-- Main Footer -->
-        @include('master.footer')
-    </div>
-    <!-- ./wrapper -->
 
-    <!-- REQUIRED SCRIPTS -->
-    @include('master.include.script')
-</body>
-
-</html>
